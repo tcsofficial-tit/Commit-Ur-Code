98ff0daae8b4291626dd600f110bf62cacda2094
NikhilCSE22 <113693242+NikhilCSE22@users.noreply.github.com>
2022-10-31 20:53:18 +0530

    Create 21CE098.java

---
+++ 30-10-2022/Solutions/2nd year/21CE098.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Solution {
+    public static int[] buildArray(int[] nums) {
+        Scanner s1=new Scanner(System.in);
+       int n=nums.length;
+        int arr[]=new int [n];
+        	for(int j=0;j<n;j++)
+	{
+		 arr[j]=nums[nums[j]];
+    }
+    return arr;
+    }
+    public static void main(String[] ars)
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter the size of array");
+	int k=s.nextInt();
+	  System.out.println("enter the elements of an array");
+	int nums[]=new int[k];
+	for(int i=0;i<k;i++)
+	{
+		nums[i]=s.nextInt();
+	}
+        int arr[]=buildArray(nums);
+        for(int j=0;j<k;j++)
+        {
+        System.out.println(arr[j]);
+    }
+    }
+}
